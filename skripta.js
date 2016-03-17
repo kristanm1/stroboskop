2736c0e8a0e26dce479d03b6ca478370bfbd475c
kristanm1 <kristanm1@gmail.com>
2016-03-17 20:56:15 +0000

    Dinamika gumba za zagon stroboskopa

--- skripta.js
+++ skripta.js
@@ -52,8 +52,8 @@ window.addEventListener('load', function() {
 			vrednosti.push(barva.innerHTML);
 		}
 		
-		minCas = 1000;
-		maxCas = 1000;
+		minCas = Number(document.querySelector("#min").value);
+		maxCas = Number(document.querySelector("#max").value);
 		spremeniBarvo(0);
 		
 		var start = document.querySelector("#start");
